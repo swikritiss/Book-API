c680e8c47fb02040cd5097412b1886b22e2ba4de
Swikriti Singh <35220557+swikritiss@users.noreply.github.com>
2021-05-21 10:45:55 +0545

    API Documentation Created

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const app = express();
 const Joi = require('joi');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const swaggerJsDoc = require('swagger-jsdoc'); //this worked after downgrading swagger-jsdoc to version 6.0.0
+const swaggerUI = require('swagger-ui-express');
+
 
 const bookRoute = require('./src/routes/route_book');
 
@@ -13,6 +16,52 @@ db.then(() => {
 
 app.use(bodyParser.json());
 app.use(express.json());
+
+const swaggerOptions = {
+    swaggerDefinition : {
+        info : {
+            title : 'Book Library API',
+            version : '1.0.0'
+        }
+    },
+    apis : ['main.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * /books:
+ *   get: 
+ *     description : get all books
+ *     responses : 
+ *       200 : 
+ *         description : Success
+ */
+
+/**
+ * @swagger
+ * /books:
+ *   post: 
+ *     description : create a book
+ *     parameters : 
+ *     - name : title
+ *       description : title of book
+ *       in : formData
+ *       type : string
+ *     - name : pages
+ *       description : pages in book
+ *       in : formData
+ *       type : number
+ *     - name : author
+ *       description : name of author of the book
+ *       in : formData
+ *       type : string
+ *     responses : 
+ *       201 : 
+ *         description : Created
+ */
 
 app.use('/books', bookRoute);
 
